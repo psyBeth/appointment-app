eae0ba999182f8f4026524c7c56f9d81d148d499
Betul <betulkoru06@gmail.com>
2024-02-11 22:36:48 +0300

    AddModal.jsx 1

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -2,8 +2,14 @@ import Container from "react-bootstrap/Container"
 import {doctorData} from "../helper/data"
 import { Col } from "react-bootstrap"
 import { Row } from "react-bootstrap"
+import AddModal from "./AddModal"
+import { useState } from 'react';
 
 const Doctors = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
@@ -11,13 +17,18 @@ const Doctors = () => {
       </h3>
       <Row>
         {doctorData.map(({id, img, dep, name}) => (
-          <Col>
-          <img src={img} alt={name} />
+          <Col key={id}>
+          <img src={img} alt={name} 
+          className="img-thumbnail doctor-img" 
+          onClick={handleShow} />
           <h5>{name}</h5>
           <h6>{dep}</h6>
           </Col>
         ))}
       </Row>
+      <AddModal 
+      handleClose={handleClose} 
+      show={show} />
     </Container>
   )
 }
