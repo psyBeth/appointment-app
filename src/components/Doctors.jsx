db47d5122248d0a0e16cddd99f07d946d31c8514
Betul <betulkoru06@gmail.com>
2024-02-11 21:44:59 +0300

    doctors row struct

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,4 +1,7 @@
 import Container from "react-bootstrap/Container"
+import {doctorData} from "../helper/data"
+import { Col } from "react-bootstrap"
+import { Row } from "react-bootstrap"
 
 const Doctors = () => {
   return (
@@ -6,6 +9,15 @@ const Doctors = () => {
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
+      <Row>
+        {doctorData.map(({id, img, dep, name}) => (
+          <Col>
+          <img src={img} alt={name} />
+          <h5>{name}</h5>
+          <h6>{dep}</h6>
+          </Col>
+        ))}
+      </Row>
     </Container>
   )
 }
