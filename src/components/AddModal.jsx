01fda504583ceb53d02a780ceb67ba725c971d6b
Betul <betulkoru06@gmail.com>
2024-02-12 12:47:42 +0300

    appointments list state

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,10 +1,16 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 function AddModal({show, handleClose, drName}) {
+  const [name, setName] = useState("")
+  const [date, setDate] = useState("")
 
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name,date);
+  }
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -12,15 +18,15 @@ function AddModal({show, handleClose, drName}) {
           <Modal.Title>Appointment for {drName} </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <Form>
+            <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
                 <Form.Label>Patient Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter Name" required />
+                <Form.Control onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter Name" required />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="date">
                 <Form.Label>Date</Form.Label>
-                <Form.Control type="datetime-local" placeholder="Date" required />
+                <Form.Control onChange={(e) => (e.target.value)} type="datetime-local" placeholder="Date" required />
             </Form.Group>
             <div className='"text-center'>
                 <Button variant="success" type="submit" className='me-2'>
--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,11 +1,33 @@
+import { Row } from "react-bootstrap"
 import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col"
+import { TiDelete } from "react-icons/ti";
 
-const AppointmentList = () => {
+const AppointmentList = ({appointments}) => {
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
+      {
+        appointments.map(({id, patient, consulted, doctor, day}) => (
+          <div key={id} className={consulted ? "appointments consulted" : "appointments"} >
+            <Row>
+              <Col>
+              <h4>{patient}</h4>
+              <h5>{doctor}</h5>
+              </Col>
+              <Col>
+              <h5> Date: {day.toLocaleDateString("tr")} </h5>
+              <h6> Time: {day.toLocaleTimeString("tr")} </h6>
+              </Col>
+              <Col>
+                <TiDelete className="text-danger fs-1" type="button" />
+              </Col>
+            </Row>
+          </div>
+        ))
+      }
     </Container>
   )
 }
