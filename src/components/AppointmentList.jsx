55747a5740bc270a580a2ec8e21cab675053b687
Betul <betulkoru06@gmail.com>
2024-02-12 13:23:53 +0300

    v7

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -11,9 +11,13 @@ const AppointmentList = ({appointments}) => {
       </h3>
       {
         appointments.map(({id, patient, consulted, doctor, day}) => (
-          <div key={id} className={consulted ? "appointments consulted" : "appointments"} >
-            <Row>
-              <Col>
+          <div 
+            key={id} 
+            className={consulted ? "appointments consulted" : "appointments"} 
+            role="button"
+          >
+            <Row className="justify-content-between align-items-center">
+              <Col md={6}>
               <h4>{patient}</h4>
               <h5>{doctor}</h5>
               </Col>
@@ -21,8 +25,10 @@ const AppointmentList = ({appointments}) => {
               <h5> Date: {day.toLocaleDateString("tr")} </h5>
               <h6> Time: {day.toLocaleTimeString("tr")} </h6>
               </Col>
-              <Col>
-                <TiDelete className="text-danger fs-1" type="button" />
+              <Col className="text-end">
+                <TiDelete 
+                  className="text-danger fs-1" 
+                  type="button" />
               </Col>
             </Row>
           </div>
