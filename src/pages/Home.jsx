9201a6b0e88857fac649c9bb6d69d18f074776d2
Betul <betulkoru06@gmail.com>
2024-02-12 23:25:50 +0300

    localStorage added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,24 @@ import { appointmentData } from "../helper/data"
 import { useState } from "react"
 
 const Home = () => {
-const [appointments, setAppointments] = useState(appointmentData)
+  // const [appointments, setAppointments] = useState(appointmentData);
+const [appointments, setAppointments] = useState(JSON.parse(localStorage.getItem("list")) || []);
+
 const handleAdd = (newAppointment) => {
-  setAppointments([...appointments, newAppointment])
+  setAppointments([...appointments, newAppointment]);
+  localStorage.setItem("list", JSON.stringify([...appointments, newAppointment]))
 };
 
   const handleDelete = (id) => {
     const filteredList = appointments.filter(item => item.id !== id)
-    setAppointments(filteredList)
+    setAppointments(filteredList);
+    localStorage.setItem("list", JSON.stringify(filteredList))
   };
 
   const handleDoubleClick = (id) => {
     const updatedList = appointments.map(item => item.id === id ? {...item, consulted: !item.consulted} : item);
     setAppointments(updatedList);
+    localStorage.setItem("list", JSON.stringify(updatedList))
   }
 
   return (
